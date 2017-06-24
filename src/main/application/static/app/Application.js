28390f253a321e5abd29ce24871e80403b1ebacd
Seven7 <Seven7@DESKTOP-UQUPKDR>
2017-06-24 22:23:04 +0800

    destroy the loader/spinner after application is up

--- src/main/application/static/app/Application.js
+++ src/main/application/static/app/Application.js
@@ -1,29 +1,27 @@
 
-
 /**
- * The main application class. An instance of this class is created by app.js when it calls
- * Ext.application(). This is the ideal place to handle application launch and initialization
- * details.
+ * The main application class. An instance of this class is created by app.js
+ * when it calls Ext.application(). This is the ideal place to handle
+ * application launch and initialization details.
  */
 Ext.define('App.Application', {
-    extend: 'Ext.app.Application',
+	extend : 'Ext.app.Application',
 
-    name: 'App',
+	name : 'App',
 
-    // The tab we want to activate if there is no "#tag" in the URL.
-    defaultToken: 'quarterly',
+	// The tab we want to activate if there is no "#tag" in the URL.
+	defaultToken : 'quarterly',
 
-    views: [
-        'App.view.main.Main'
-    ],
+	views : [ 'App.view.main.Main' ],
 
-    launch: function () {
-    	Ext.fly('loader');
-        Ext.direct.Manager.addProvider(Ext.app.REMOTING_API);
-        // Let's add a CSS class to body if flex box wrap is not implemented or broken
-        // http://flexboxlayouts.com/flexboxlayout_tricks.html
-        if (Ext.browser.is.Gecko && Ext.browser.version.major < 28) {
-            Ext.getBody().addCls('x-flex-wrap-broken');
-        }
-    }
+	launch : function() {
+		Ext.fly('loader').destroy();
+		Ext.direct.Manager.addProvider(Ext.app.REMOTING_API);
+		// Let's add a CSS class to body if flex box wrap is not implemented or
+		// broken
+		// http://flexboxlayouts.com/flexboxlayout_tricks.html
+		if (Ext.browser.is.Gecko && Ext.browser.version.major < 28) {
+			Ext.getBody().addCls('x-flex-wrap-broken');
+		}
+	}
 });
