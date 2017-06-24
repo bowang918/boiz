52acecd4ecb27cab83392a26afc7c6431bd4d3e4
Seven7 <Seven7@DESKTOP-UQUPKDR>
2017-06-24 18:44:23 +0800

    move feed list out from quarterly report view

---
+++ src/main/application/static/app/view/feed/FeedListList.js
@@ -0,0 +1,28 @@
+Ext.define('App.view.feed.FeedList', {
+    extend: 'Ext.view.View',
+    alias: 'widget.feedlist',
+
+    cls: 'quarterly-dataview',
+
+    itemSelector: 'div.thumb-wrap',
+
+    tpl: [
+        '<tpl for=".">',
+        // Break every four quarters
+            '<tpl if="xindex % 4 === 1">',
+                '<div class="statement-type">{[Ext.Date.format(values.uploaded, Ext.Date.patterns.YearMonthZh)]}</div>',
+            '</tpl>',
+
+            '<div class="thumb-wrap">',
+                '<a class="thumb" href="{url}" target="_blank">',
+                    '<div class="thumb-icon {type}"></div>',
+                    '<div class="thumb-title-container">',
+                    '<div class="thumb-title">{fileName}</div>',
+                    '<div class="thumb-title-small">上传于: {[Ext.Date.format(values.uploaded, Ext.Date.patterns.ISO8601Long)]}</div>',
+                    '</div>',
+                    '<div class="thumb-download"></div>',
+                '</a>',
+            '</div>',
+        '</tpl>'
+    ]
+});
