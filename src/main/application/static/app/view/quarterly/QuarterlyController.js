5f1cc45dd2d5dc930e33a7bc300b3c75d31d3299
Seven7 <Seven7@DESKTOP-UQUPKDR>
2017-06-24 15:01:25 +0800

    add uploading success callback to show message

--- src/main/application/static/app/view/quarterly/QuarterlyController.js
+++ src/main/application/static/app/view/quarterly/QuarterlyController.js
@@ -1,35 +1,38 @@
 Ext.define('App.view.quarterly.QuarterlyController', {
-    extend: 'Ext.app.ViewController',
-    alias: 'controller.quarterly',
+	extend : 'Ext.app.ViewController',
+	alias : 'controller.quarterly',
 
-    init: function (view) {
-        // We provide the updater for the activeState config of our View.
-        view.updateActiveState = this.updateActiveState.bind(this);
-    },
+	init : function(view) {
+		// We provide the updater for the activeState config of our View.
+		view.updateActiveState = this.updateActiveState.bind(this);
+	},
 
-    menuItemClick: function (button, menuitem) {
-        var view = this.getView();
-        view.setActiveState(menuitem.text);
-    },
+	menuItemClick : function(button, menuitem) {
+		var view = this.getView();
+		view.setActiveState(menuitem.text);
+	},
 
-    updateActiveState: function (activeState) {
-        var viewModel = this.getViewModel();
+	updateActiveState : function(activeState) {
+		var viewModel = this.getViewModel();
 
-        viewModel.set('company', activeState);
+		viewModel.set('company', activeState);
 
-        this.fireEvent('changeroute', this, 'quarterly/' + activeState);
-    },
+		this.fireEvent('changeroute', this, 'quarterly/' + activeState);
+	},
 
-    uploadFeed:function(fileField, value){
-        var form = fileField.up('form');
-        if (form.isValid()) {
-            console.log(value);
-            form.submit({
-                waitMsg: '文件上传中...',
-                success: function(form) {
-                    form.reset();
-                }
-            });
-        }
-    }
+	uploadFeed : function(fileField, value) {
+		var form = fileField.up('form');
+		if (form.isValid()) {
+			console.log(value);
+			form.submit({
+				waitMsg : '文件上传中...',
+				success : function(form, action) {
+					Ext.Msg.alert('Success', action.result.msg);
+				},
+				failure : function(form, action) {
+					Ext.Msg.alert('Failed', action.result.msg);
+				}
+			});
+		}
+	}
 });
